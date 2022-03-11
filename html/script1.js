278baa1ad76db074534af2040fc6aea863173de7
Kashish <kp25179@gmail.com>
2022-03-11 09:19:25 +0800

    2

---
+++ html/script1.js
@@ -0,0 +1,10 @@
+function submitMyform() {
+    let form = document.forms["myform"]
+
+    let name = form['Name'].value
+    let email = form['Name'].value
+    let date = form['Name'].value
+
+    console.log()
+    alert("/name" + name)
+}
